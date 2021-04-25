64a3cb2e255d2c712b6d094f354657a1d549295f
JY <kims2606@gmail.com>
2021-04-25 19:16:12 +0900

    Component, scoped styles, global styles

--- my-first-nuxt-app/layouts/users.vue
+++ my-first-nuxt-app/layouts/users.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
+    <Header />
     <header>USERS SECTION</header>
     <Nuxt />
   </div>
 </template>
 
 <script>
+import Header from '~/components/Header.vue'
+
 export default {
+  components: {
+    Header
+  },
   layout: 'users'
 }
 </script>
