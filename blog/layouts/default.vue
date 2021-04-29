41b6283b2a9ce9d67e070350d323861d68328c65
JY <kims2606@gmail.com>
2021-04-29 14:05:40 +0900

    blog project header and Navigation

--- blog/layouts/default.vue
+++ blog/layouts/default.vue
@@ -1,15 +1,35 @@
 <template>
-  <div>
-    <Nuxt />
-  </div>
+    <div>
+        <the-header @sidenavToggle="displaySidenav = !displaySidenav"/>
+        <the-sidenav 
+            :show="displaySidenav" 
+            @close="displaySidenav = false"/>
+        <nuxt />
+    </div>
 </template>
+
+<script>
+import TheHeader from '../components/Navigation/TheHeader'
+import TheSidenav from '../components/Navigation/TheSidenav'
+
+export default {
+    components: {
+        TheHeader, TheSidenav
+    },
+    data() {
+        return {
+            displaySidenav : false
+        }
+    }
+}
+</script>
 
 <style>
 html {
-  font-family: 'Open Sans', sans-serif;
+    font-family: 'Open Sans', sans-serif;
 }
 
 body {
-  margin: 0;
+    margin: 0;
 }
 </style>
