80db151674a0bcd78e4395a0c8e0e905a372fd18
JY <kims2606@gmail.com>
2021-05-06 08:51:55 +0900

    blog project asyncData 메서드 context.params 활용

--- blog/pages/posts/_id/index.vue
+++ blog/pages/posts/_id/index.vue
@@ -1,18 +1,39 @@
 <template>
     <div class="single-post-page">
         <section class="post">
-            <h1 class="post-title">Title of the Post</h1>
+            <h1 class="post-title">{{ loadedPost.title }}</h1>
             <div class="post-details">
-                <div class="post-detail">Last updated on XXX</div>
-                <div class="post-detail">Written by NAME</div>
+                <div class="post-detail">Last updated on {{ loadedPost.updatedDate }}</div>
+                <div class="post-detail">Written by {{ loadedPost.author }}</div>
             </div>
-            <p class="post-content">Content of the post</p>
+            <p class="post-content">{{ loadedPost.content }}</p>
         </section>
         <section class="post-feedback">
             <p>Let me know What you think about the post, send a mail to <a href="mailto:feedback@my-awesome-domain.com">feedback@my-awesome-domain.com</a></p>
         </section>
     </div>
 </template>
+
+<script>
+export default {
+    asyncData(context, callback) {
+        setTimeout(() => {
+            callback(null, {
+                loadedPost: {
+                    id : "1",
+                    title : `First Post (ID:${context.params.id})`,
+                    previewText : "This my first post!",
+                    author: 'Joonyeop',
+                    updatedDate : new Date(),
+                    content: 'dummy content',
+                    thumbnail : require('~/assets/images/img.jpeg')
+                }
+            });
+        }, 1000);
+    }
+  
+}
+</script>
 
 <style scoped>
 .single-post-page {
